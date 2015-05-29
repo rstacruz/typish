d1a0c1d7f715201bd765b9ca608f1d6d68282c6f
Rico Sta. Cruz <hi@ricostacruz.com>
2015-05-29 19:25:47 +0800

    Simplify umd loader

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 ;(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(factory);
-  } else if (typeof exports === 'object') {
-    module.exports = factory();
-  } else {
-    root.typish = factory();
-  }
+  if (typeof define === 'function' && define.amd) define(factory);
+  else if (typeof exports === 'object') module.exports = factory();
+  else root.typish = factory();
 }(this, function () {
 
   /***
